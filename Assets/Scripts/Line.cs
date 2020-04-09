22628395d8ef95212aa2f25f9946c46c312b6a11
seba_almasy <andres.urrea.ap@gmail.com>
2020-04-09 17:21:27 -0300

    Mecanica Linerendere

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Line : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public LineRenderer _line;
+    public EdgeCollider2D _line_col;
+    public int _large;
+    
+
+    List<Vector2> points;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLine(Vector2 mousePos) {
+
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), mousePos) >.1f && _large > points.Count)
+        {
+            SetPoint(mousePos);
+        }
+    }
+
+
+    void SetPoint(Vector2 point)
+    {
+
+        points.Add(point);
+
+        _line.positionCount = points.Count;
+        _line.SetPosition(points.Count-1,point);
+        if (points.Count > 1)
+        {
+            _line_col.points = points.ToArray();
+            LineCreator.instance.UpdateBar(points.Count);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "bonus")
+        {
+            Debug.Log("PUEDE");
+            collision.gameObject.SetActive(false);
+            LineCreator.instance.CreateNewLine(true, collision.GetComponent<SpriteRenderer>().color);
+
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LineCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static LineCreator instance;
+
+    public GameObject linePrefab;//,trailPrefab;
+    Line activeLine;
+    bool _can_draw;
+
+    public Image _energy_bar;
+
+    GameObject lineGo,trailGo;
+
+
+
+    List<Line> _lines;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        _can_draw = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //lineGo = Instantiate(linePrefab);
+            CreateNewLine(false , Color.white);
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null && _can_draw)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+            //trailGo.transform.position = mousePos;
+        }
+        
+    }
+
+    public void UpdateBar(float used) {
+
+        _energy_bar.fillAmount = (200 - used) / 200;
+
+    }
+
+    public void CreateNewLine(bool from_bonus,Color _color) {
+
+        //activeLine = null;
+        if (from_bonus)
+        {
+            activeLine = null;
+        }
+        lineGo = Instantiate(linePrefab);
+        //trailGo = Instantiate(trailPrefab);
+
+        activeLine = lineGo.GetComponent<Line>();
+        activeLine._line.SetColors(_color,_color);
+
+    }
+
+
+
+}
