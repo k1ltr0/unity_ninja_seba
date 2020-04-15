46cfb2e83fa62448b88bab04e9ae877a3bdbe9b7
Ricardo Silva <ricardo@loadingplay.com>
2020-04-15 15:18:17 -0400

    better collisiion detection

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -12,22 +12,107 @@ public class EnemyCollision : MonoBehaviour
     private GameObject top;
     private GameObject right;
     private GameObject bottom;
+    private GameObject line_from;
+    private GameObject line_to;
     private LineRenderer[] lines;
 
-    private Vector2 FindIntersection(Vector2 s1, Vector2 e1, Vector2 s2, Vector2 e2) {
-        float a1 = e1.y - s1.y;
-        float b1 = s1.x - e1.x;
-        float c1 = a1 * s1.x + b1 * s1.x;
+    public static bool LineIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, ref Vector2 intersection)
+    {
+        float Ax, Bx, Cx, Ay, By, Cy, d, e, f, num, offset;
+        float x1lo, x1hi, y1lo, y1hi;
 
-        float a2 = e2.y - s2.y;
-        float b2 = s2.x - e2.x;
-        float c2 = a2 * s2.x + b2 * s2.y;
+        Ax = p2.x - p1.x;
+        Bx = p3.x - p4.x;
 
-        float delta = a1 * b2 - a2 * b1;
-        //If lines are parallel, the result will be (NaN, NaN).
-        return delta == 0 ? new Vector2(1000, 1000)
-            : new Vector2((b2 * c1 - b1 * c2) / delta, (a1 * c2 - a2 * c1) / delta);
+        // X bound box test/
+        if (Ax < 0)
+        {
+            x1lo = p2.x; x1hi = p1.x;
+        }
+        else
+        {
+            x1hi = p2.x; x1lo = p1.x;
+        }
+
+        if (Bx > 0)
+        {
+            if (x1hi < p4.x || p3.x < x1lo) return false;
+        }
+        else
+        {
+            if (x1hi < p3.x || p4.x < x1lo) return false;
+        }
+
+        Ay = p2.y - p1.y;
+        By = p3.y - p4.y;
+
+        // Y bound box test//
+        if (Ay < 0)
+        {
+            y1lo = p2.y; y1hi = p1.y;
+        }
+        else
+        {
+            y1hi = p2.y; y1lo = p1.y;
+        }
+
+        if (By > 0)
+        {
+            if (y1hi < p4.y || p3.y < y1lo) return false;
+        }
+        else
+        {
+            if (y1hi < p3.y || p4.y < y1lo) return false;
+        }
+
+        Cx = p1.x - p3.x;
+        Cy = p1.y - p3.y;
+        d = By * Cx - Bx * Cy;  // alpha numerator//
+        f = Ay * Bx - Ax * By;  // both denominator//
+
+        // alpha tests//
+        if (f > 0)
+        {
+            if (d < 0 || d > f) return false;
+        }
+        else
+        {
+            if (d > 0 || d < f) return false;
+        }
+
+        e = Ax * Cy - Ay * Cx;  // beta numerator//
+
+        // beta tests //
+        if (f > 0)
+        {
+            if (e < 0 || e > f) return false;
+        }
+        else
+        {
+            if (e > 0 || e < f) return false;
+        }
+
+        // check if they are parallel
+        if (f == 0) return false;
+
+        // compute intersection coordinates //
+        num = d * Ax; // numerator //
+        offset = same_sign(num, f) ? f * 0.5f : -f * 0.5f;   // round direction //
+        intersection.x = p1.x + (num + offset) / f;
+
+        num = d * Ay;
+        offset = same_sign(num, f) ? f * 0.5f : -f * 0.5f;
+        intersection.y = p1.y + (num + offset) / f;
+
+        return true;
     }
+
+    private static bool same_sign(float a, float b)
+    {
+        return ((a * b) >= 0f);
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +122,8 @@ public class EnemyCollision : MonoBehaviour
         right = Instantiate(collision_point);
         bottom = Instantiate(collision_point);
         left = Instantiate(collision_point);
+        line_from = Instantiate(collision_point);
+        line_to = Instantiate(collision_point);
     }
 
     // Update is called once per frame
@@ -54,43 +141,48 @@ public class EnemyCollision : MonoBehaviour
                 Vector2 line_1_from = line.GetPosition(0);
                 Vector2 line_1_to = line.GetPosition(1);
 
-                Vector2 square_top_left = transform.TransformPoint(new Vector2(
-                    collider2D.offset.x - collider2D.size.x * .5f,
-                    collider2D.offset.y + collider2D.size.y * .5f
-                ));
-                Vector2 square_top_right = transform.TransformPoint(new Vector2(
-                    collider2D.offset.x + collider2D.size.x * .5f,
-                    collider2D.offset.y + collider2D.size.y * .5f
-                ));
-                Vector2 square_bottom_left = transform.TransformPoint(new Vector2(
-                    collider2D.offset.x - collider2D.size.x * .5f,
-                    collider2D.offset.y - collider2D.size.y * .5f
-                ));
-                Vector2 square_bottom_right = transform.TransformPoint(new Vector2(
-                    collider2D.offset.x + collider2D.size.x * .5f,
-                    collider2D.offset.y - collider2D.size.y * .5f
-                ));
-
-                Vector2 intersection_top = FindIntersection(
-                    line_1_from, line_1_to,
-                    square_top_left, square_top_right
+                Vector2 square_top_left = transform.TransformPoint(
+                    collider2D.offset + Vector2.Reflect(collider2D.size, Vector2.left) * .5f
+                );
+                Vector2 square_top_right = transform.TransformPoint(
+                    collider2D.offset + collider2D.size * .5f
+                );
+                Vector2 square_bottom_left = transform.TransformPoint(
+                    collider2D.offset - collider2D.size * .5f
+                );
+                Vector2 square_bottom_right = transform.TransformPoint(
+                    collider2D.offset + Vector2.Reflect(collider2D.size, Vector2.down) * .5f
                 );
 
-                Vector2 intersection_right = FindIntersection(
-                    line_1_from, line_1_to,
-                    square_top_right, square_bottom_right
+                Vector2 intersection_top = Vector2.zero;
+                LineIntersection(
+                    line_1_from, line_1_to, square_top_left, square_top_right,
+                    ref intersection_top
                 );
 
-                Vector2 intersection_bottom = FindIntersection(
+                Vector2 intersection_right = Vector2.zero;
+                LineIntersection(
                     line_1_from, line_1_to,
-                    square_bottom_left, square_bottom_right
+                    square_top_right, square_bottom_right,
+                    ref intersection_right
                 );
 
-                Vector2 intersection_left = FindIntersection(
+                Vector2 intersection_bottom = Vector2.zero;
+                LineIntersection(
                     line_1_from, line_1_to,
-                    square_top_left, square_bottom_left
+                    square_bottom_left, square_bottom_right,
+                    ref intersection_bottom
                 );
 
+                Vector2 intersection_left = Vector2.zero;
+                LineIntersection(
+                    line_1_from, line_1_to,
+                    square_top_left, square_bottom_left,
+                    ref intersection_left
+                );
+
+                line_from.transform.position = line_1_from;
+                line_to.transform.position = line_1_to;
                 top.transform.position = intersection_top;
                 right.transform.position = intersection_right;
                 bottom.transform.position = intersection_bottom;
